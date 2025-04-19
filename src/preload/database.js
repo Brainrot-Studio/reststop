37acd0607b6117f9c3d194de621c575b5f853848
MrJesseh <wjesse12@gmail.com>
2025-04-19 18:46:08 -0400

    Added more jsdocs

--- src/preload/database.js
+++ src/preload/database.js
@@ -2,6 +2,16 @@ const { expose } = require('./utils/expose.js');
 const RequestHistory = require('../database/requestHistory.js');
 
 expose('DatabaseAPI', {
+  /**
+   * Gets all of a user's request history
+   * @returns {Promise<Object[]>} The response object.
+  */
   getHistory: async () => await RequestHistory.getHistory(),
+  /**
+   * Adds an entry to the user's history save file
+   * @param {Object} req The request options object.
+   * @param {Object} res The response data.
+   * @returns {Promise<Boolean>} The response object.
+  */
   addHistory: async (data) => await RequestHistory.addHistory(data)
 });
--- src/preload/requests.js
+++ src/preload/requests.js
@@ -2,5 +2,14 @@ const { expose } = require('./utils/expose.js');
 const sendRequest = require('../core/sendRequest.js');
 
 expose('RequestAPI', {
+  /**
+   * Sends a request via Axios
+   * @param {Object} options The request options object
+   * @param {String} options.url The request url
+   * @param {String} options.method The HTTP request type
+   * @param {Object} options.headers The HTTP request headers
+   * @param {Object|String} options.body The HTTP request body
+   * @returns {Promise<Object>} The response object.
+  */
   sendRequest: async (data) => await sendRequest(data)
 });
